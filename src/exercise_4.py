d9954768dff4c2ab150e091362e95bc9f5c229dd
PONSASIKALA <84910528+PONSASIKALA@users.noreply.github.com>
2021-06-11 15:44:45 +0200

    Add files via upload

---
+++ src/exercise_4.py
@@ -0,0 +1,18 @@
+# routes.py
+from flask import Flask, request
+
+my_app = Flask(__name__)
+
+
+@my_app.route("/", methods=["GET"])
+def hello():
+    return "Hello Hello hello World world!"
+
+
+@my_app.route("/", methods=["POST"])
+def mirror_data():
+    data = request.get_json()
+    return data
+
+
+my_app.run()
