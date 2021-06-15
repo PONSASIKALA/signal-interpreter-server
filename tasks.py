41add301439d21602ba65a75534ea0ca6c6b5de5
PONSASIKALA <84910528+PONSASIKALA@users.noreply.github.com>
2021-06-15 10:51:18 +0200

    Add files via upload

--- tasks.py
+++ tasks.py
@@ -1,20 +1,26 @@
 import os
 from subprocess import call
 from invoke import task
+
 CURR_DIR = os.path.abspath(os.path.dirname(__file__))
-SRC_DIR = os.path.join(CURR_DIR,"my-python-project")
-UNIT_TEST_DIR = os.path.join(CURR_DIR,"tests","unit")
-COV_PATH = os.path.join(CURR_DIR,".coveragerc")
+""" signal-interpreter-server while running in local PC"""
+SRC_DIR = os.path.join(CURR_DIR, "tests")
+UNIT_TEST_DIR = os.path.join(CURR_DIR, "tests", "unit")
+COV_PATH = os.path.join(CURR_DIR, ".coveragerc")
 
 @task
 def style(_):
-	call(f"pycodestyle{SRC_DIR}--ignore=E501",shell=True)
+    call(f"pycodestyle {SRC_DIR} --ignore=E501", shell=True)
 
 @task
 def lint(_):
-	call(f"pylint {SRC_DIR}",shell=True)
+    call(f"pylint {SRC_DIR}", shell=True)
 
 @task
 def unit_test(_):
-	cmd = f"pytest {UNIT_TEST_DIR}--cov{SRC_DIR}--cov-config={COV_PATH}"
-	call(cmd,shell=True)
+    call(f"pytest {UNIT_TEST_DIR} --cov {SRC_DIR} --cov-config={COV_PATH} --verbose")
+    return_value = call(cmd, shell=True)
+    if return_value == 0:
+        print("Success!")
+    else:
+        print(f'Failed to run:\n"{cmd}"\nwith exit code: {return_value}')
