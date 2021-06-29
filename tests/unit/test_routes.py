92bee5c7f3b5226d284b6608dc65d9c1c131ae58
PONSASIKALA <84910528+PONSASIKALA@users.noreply.github.com>
2021-06-29 10:50:46 +0200

    Add files via upload

--- tests/unit/test_routes.py
+++ tests/unit/test_routes.py
@@ -1,15 +1,26 @@
+""" Unit tests for routes.py """
 from unittest.mock import patch
 
-from signal_interpreter_server.routes import signal_interpreter_app, JsonParser
+from signal_interpreter_server.exceptions import JsonParserError
+from signal_interpreter_server.json_parser import JsonParser
 
 
-def test_interpret_signal():
-    signal_interpreter_app.testing = True
-    signal_interpreter_app_instance = signal_interpreter_app.test_client()
-
-    with patch.object(JsonParser, "get_signal_title", return_value="ECU Reset") as mock_get_signal_title:
+def test_interpret_signal_with_valid_signal(signal_interpreter_app_instance):
+    with patch.object(JsonParser, "get_signal_title", return_value="") as mock_get_signal_title:
         with signal_interpreter_app_instance as client:
-            payload = {"signal": "11"}
-            response = client.post("/", json=payload)
+            payload = {"signal": "99"}
+            client.post("/", json=payload)
             mock_get_signal_title.assert_called_with("11")
-            assert response.get_json() == "ECU Reset"
+
+
+def test_interpret_signal_with_invalid_key(signal_interpreter_app_instance):
+    with signal_interpreter_app_instance as client:
+        payload = {"dummy": "11"}
+        assert client.post("/", json=payload).status_code == 400
+
+
+def test_interpret_signal_with_invalid_identifier(signal_interpreter_app_instance):
+    with patch.object(JsonParser, "get_signal_title", side_effect=JsonParserError):
+        with signal_interpreter_app_instance as client:
+            payload = {"signal": "98"}
+            assert client.post("/", json=payload).status_code == 404
