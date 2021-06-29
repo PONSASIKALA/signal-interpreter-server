eaa99042d1201ed817150d99cad19b93f15b4479
PONSASIKALA <84910528+PONSASIKALA@users.noreply.github.com>
2021-06-29 11:30:27 +0200

    Add files via upload

--- tasks.py
+++ tasks.py
@@ -1,4 +1,6 @@
+""" Tasks to perform style, lint and test """
 import os
+from subprocess import call
 from invoke import task
 
 CURR_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -7,23 +9,27 @@ SRC_DIR = os.path.join(CURR_DIR, "signal_interpreter_server")
 UNIT_TEST_DIR = os.path.join(CURR_DIR, "tests", "unit")
 COV_PATH = os.path.join(CURR_DIR, ".coveragerc")
 EXER_DIR = os.path.join(UNIT_TEST_DIR, "exercises")
+LOG_SETTINGS_PATH = os.path.join(CURR_DIR, "..", "cfg", "log_settings.yaml")
+FIXTURE_PATH = os.path.join(CURR_DIR, "..", "fixture", "test_basic.json")
+INTEGRATION_TEST_DIR = os.path.join(CURR_DIR, "tests", "integration")
 
 @task
-def style(c):
-    c.run(f"pycodestyle {SRC_DIR} --ignore=E501")
-
-
-@task
-def lint(c):
-    c.run(f"pylint {SRC_DIR}")
-
+def style(_):
+    """ Style check """
+    call(f"pycodestyle {SRC_DIR} --ignore=E501", shell=True)
 
 @task
-def unit_test(c):
-    ignore_ex = f"--ignore {EXER_DIR}"
-    return_value = c.run(f"pytest {UNIT_TEST_DIR} --cov {SRC_DIR} --cov-config={COV_PATH} --verbose {ignore_ex}")
-    
-    if return_value == 0:
-        print("Success!")
-    else:
-        print(f'Failed to run:\n"{cmd}"\nwith exit code: {return_value}')
+def lint(_):
+    """ Lint check """
+    call(f"pylint {SRC_DIR}", shell=True)
+
+@task
+def unit_test(_):
+    """ Run unit tests """
+    call(f"pytest {UNIT_TEST_DIR} --cov {SRC_DIR} --cov-config={COV_PATH} --verbose")
+	
+	
+@task
+def integration_test(_):
+    """ Run integration tests """
+    call(f"pytest {INTEGRATION_TEST_DIR} --verbose")
