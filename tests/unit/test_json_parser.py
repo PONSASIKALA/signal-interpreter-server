d3c10147f7625b256163e30c64265c653ebecce2
PONSASIKALA <84910528+PONSASIKALA@users.noreply.github.com>
2021-06-29 10:49:21 +0200

    Add files via upload

--- tests/unit/test_json_parser.py
+++ tests/unit/test_json_parser.py
@@ -1,7 +1,10 @@
+""" Unit tests for json_parser.py """
 from unittest.mock import patch, mock_open
 
-from signal_interpreter_server.json_parser import JsonParser
+import pytest
 
+from signal_interpreter_server.exceptions import JsonParserError
+from signal_interpreter_server.json_parser import JsonParser
 
 VALID_JSON_DATA = '{ "json" : "This is a JSON" }'
 PARSED_JSON_DATA = {"json": "This is a JSON"}
@@ -14,7 +17,19 @@ def test_load_file():
         assert json_parser.data == PARSED_JSON_DATA
 
 
-def test_get_signal_title():
-    json_parser = JsonParser()
-    json_parser.data = {"services": [{"title": "ECU Reset", "id": "11"}]}
-    assert json_parser.get_signal_title("11") == "ECU Reset"
+def test_load_file_with_invalid_file(json_parser_instance):
+    with pytest.raises(JsonParserError):
+        json_parser_instance.load_file("invalid/file/path")
+
+
+@pytest.mark.parametrize("identifier, expected_result", [
+    ("99", "ECU Reset"),
+    ("27", "Security Access")
+])
+def test_get_signal_title_with_valid_identifier(identifier, expected_result, json_parser_instance):
+    assert json_parser_instance.get_signal_title(identifier) == expected_result
+
+
+def test_get_signal_title_with_invalid_identifier(json_parser_instance):
+    with pytest.raises(JsonParserError):
+        json_parser_instance.get_signal_title("99")
