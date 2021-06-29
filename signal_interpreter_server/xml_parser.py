ce5e28cca65d132bcc67fc9d2c273965a722a8a3
PONSASIKALA <84910528+PONSASIKALA@users.noreply.github.com>
2021-06-29 16:06:20 +0200

    Update xml_parser.py

--- signal_interpreter_server/xml_parser.py
+++ signal_interpreter_server/xml_parser.py
@@ -19,11 +19,11 @@ class XmlParser:
 
     def load_file(self, file_path):
         """ Load file """
-		try:
-        	tree = ET.parse(file_path)
-			data = tree.getroot()
-			xml_string = ET.tostring(data, encoding="utf-8", method="xml")
-			data = dict(xmltodict.parse(xml_string))
+	    try:
+		    tree = ET.parse(file_path)
+	        data = tree.getroot()
+	        xml_string = ET.tostring(data, encoding="utf-8", method="xml")
+            data = dict(xmltodict.parse(xml_string))
         except FileNotFoundError as err:
             logger.exception('Could not find "%s"', file_path)
             raise XmlParserError from err
