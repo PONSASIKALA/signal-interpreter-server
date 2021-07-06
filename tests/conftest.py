ca0329a3972deed5da669b53195aa632108240f5
PONSASIKALA <84910528+PONSASIKALA@users.noreply.github.com>
2021-07-06 10:20:22 +0200

    Add files via upload

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,14 +4,19 @@
 import pytest
 
 from signal_interpreter_server.json_parser import JsonParser
-from signal_interpreter_server.xml_parser import ParserFactory
 from signal_interpreter_server.routes import signal_interpreter_app
-
+from signal_interpreter_server.parser_factory import ParserFactory
+from signal_interpreter_server.xml_parser import XmlParser
 
 @pytest.fixture
 def signal_interpreter_app_instance():
     signal_interpreter_app.testing = True
     return signal_interpreter_app.test_client()
+
+@pytest.fixture
+def parser_factory_instance():
+    return ParserFactory()
+
 
 @pytest.fixture
 def json_parser_instance():
@@ -21,6 +26,6 @@ def json_parser_instance():
 
 @pytest.fixture
 def xml_parser_instance():
-    xml_parser = ParserFactory()
+    xml_parser = XmlParser()
     xml_parser.data = {"services": [{"title": "ECU Reset", "id": "11"}, {"title": "Security Access", "id": "27"}]}
     return xml_parser
